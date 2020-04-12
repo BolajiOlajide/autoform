65aef80a0dea15469ea713d2fa9554690f82cac8
Bolaji Olajide <cooproton@gmail.com>
2020-04-13 00:34:30 +0100

    add comment

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 
 const firebaseApp = firebase.initializeApp({
+  // use your own creds, I've deleted this haha!
   apiKey: "AIzaSyBndnzAvFfYzWcEq85n_lW-fAXxa0mp1lQ",
   authDomain: "autoform-361ed.firebaseapp.com",
   databaseURL: "https://autoform-361ed.firebaseio.com",

04036b8e1cdcb79521689a27672b80bebf8c63fe
Bolaji Olajide <cooproton@gmail.com>
2020-04-13 00:22:48 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,68 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h2>Firebase AutoForm</h2>
+    <!-- {{ firebaseData }} -->
+
+    <form @submit.prevent="updateFirebase">
+      <input placeholder="your name" type="text" name="name" v-model="formData.name" />
+      <br />
+      <input placeholder="your email" type="email" name="email" v-model="formData.email" />
+      <br />
+      <input placeholder="your phone number" type="tel" name="phone" v-model="formData.phone" />
+      <br />
+      <button type="submit">Submit</button>
+    </form>
+
+    <h5>footer</h5>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { db } from './firebase';
+
+const documentPath = 'contacts/jeff';
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      firebaseData: null,
+      formData: {},
+      state: 'loading'
+    };
+  },
+
+  firestore() {
+    return {
+      firebaseData: db.doc(documentPath)
+    }
+  },
+
+  methods: {
+    async updateFirebase() {
+      try {
+        await db.doc(documentPath).set(this.formData);
+        this.state = 'synced';
+      } catch (error) {
+        this.state = 'error';
+        this.errorMessage = JSON.stringify(error);
+      }
+    }
+  },
+
+  created: async function () {
+    const docRef = db.doc(documentPath);
+
+    let data = (await docRef.get()).data();
+
+    if (!data) {
+      data = { name: '', phone: '', email: '' }
+    }
+
+    this.formData = data;
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseApp = firebase.initializeApp({
+  apiKey: "AIzaSyBndnzAvFfYzWcEq85n_lW-fAXxa0mp1lQ",
+  authDomain: "autoform-361ed.firebaseapp.com",
+  databaseURL: "https://autoform-361ed.firebaseio.com",
+  projectId: "autoform-361ed",
+  storageBucket: "autoform-361ed.appspot.com",
+  messagingSenderId: "258218298635",
+  appId: "1:258218298635:web:e366ecffaa8f39ddde660e"
+});
+
+const db = firebaseApp.firestore();
+
+export { db };
